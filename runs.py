d05ae771d33efe3161006e46c15cb900fc0495e1
Marcelo Colares <marcelocolares@MacBook-Air-de-Marcelo.local>
2025-10-07 08:11:05 -0300

    Insert chart

--- runs.py
+++ runs.py
@@ -18,7 +18,7 @@ X_train, X_test, y_train, y_test = train_test_split(X,y)
 accs = []
 for i in range(0,100):
     X_train, X_test, y_train, y_test = train_test_split(X,y)
-    model = NearestNeighbor(p=0.5)
+    model = NearestNeighbor(p=2)
     model.fit(X_train, y_train)
     predictions = model.predict(X_test)
     acc = calc_accuracy(y_test,predictions)
@@ -28,5 +28,7 @@ mean_acc, std_acc, max_acc, min_acc, median_acc = stats(accs)
 
 print(f"Mean: {mean_acc:.4f}, Std: {std_acc:.4f}, Max: {max_acc:.4f}, Min: {min_acc:.4f}, Median: {median_acc:.4f}")
 plt.figure()
-plt.plot(accs)
+plt.plot(accs,color="black")
+plt.xlabel("Iteration")
+plt.ylabel("Accuracy (%)")
 plt.show()
