0e94f06abb142aa2eb0567a03551968624fd1c97
Marcelo Colares <marcelocolares@MacBook-Air-de-Marcelo.local>
2025-10-08 21:00:18 -0300

    Insert confusion matrix

--- metrics.py
+++ metrics.py
@@ -1,9 +1,10 @@
 import numpy as np
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
 def calc_accuracy(y_true, y_predicted):
     correct = sum(yt == yp for yt, yp in zip(y_true, y_predicted))
     return round(100*[correct / len(y_true)][0][0],2)
-
 
 
 def stats(accs):
@@ -13,3 +14,11 @@ def stats(accs):
     min_acc = np.min(accs), 
     median_acc = np.median(accs)
     return float(mean_acc[0]), float(std_acc[0]),  float(max_acc[0]), float(min_acc[0]), float(median_acc)
+
+def plot_confusion_matrix(y_true,y_predicted,title):
+    plt.figure()
+    cm = confusion_matrix(y_true, y_predicted)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot(cmap=plt.cm.Blues)
+    plt.title(title)
+    plt.show()
--- runs.py
+++ runs.py
@@ -2,10 +2,11 @@ from train_test_split import train_test_split
 from dataset import load_data
 from metrics import calc_accuracy
 from nn_clf import NearestNeighbor
-from metrics import stats
+from metrics import stats, plot_confusion_matrix
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+
 
 start = time.time()
 X,y = load_data()
@@ -16,15 +17,22 @@ X_train, X_test, y_train, y_test = train_test_split(X,y)
 
 #### NN CLASSIFIFER #####
 accs = []
+predictions_per_round = []
+true_labels_per_round = []
 for i in range(0,100):
     X_train, X_test, y_train, y_test = train_test_split(X,y)
     model = NearestNeighbor(p=2)
     model.fit(X_train, y_train)
     predictions = model.predict(X_test)
+    predictions_per_round.append(predictions)
+    true_labels_per_round.append(y_test)
     acc = calc_accuracy(y_test,predictions)
     accs.append(acc)
 
 mean_acc, std_acc, max_acc, min_acc, median_acc = stats(accs)
+best_round_index = np.argmax(accs)
+worse_round_index = np.argmin(accs)
+
 
 print(f"Mean: {mean_acc:.4f}, Std: {std_acc:.4f}, Max: {max_acc:.4f}, Min: {min_acc:.4f}, Median: {median_acc:.4f}")
 plt.figure()
@@ -32,3 +40,12 @@ plt.plot(accs,color="black")
 plt.xlabel("Iteration")
 plt.ylabel("Accuracy (%)")
 plt.show()
+
+### confusion matrix ####
+y_true = true_labels_per_round[best_round_index]
+y_predicted = predictions_per_round[best_round_index]
+plot_confusion_matrix(y_true,y_predicted,"Best Confusion Matrix")
+
+y_true = true_labels_per_round[worse_round_index]
+y_predicted = predictions_per_round[worse_round_index]
+plot_confusion_matrix(y_true,y_predicted,"Worse Confusion Matrix")
